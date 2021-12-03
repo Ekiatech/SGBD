2084bc2bb750a0303ffa29753660dc03f85f8f64
Coralie Deplanne <cdeplanne001@enseirb-matmeca.fr>
2021-12-03 19:30:34 +0100

    modifs sur le menu

--- tmp/velos_station.php
+++ tmp/velos_station.php
@@ -11,42 +11,65 @@
         
         <!-- Type de la requete -->
         <div>
-            <h1>Listes des v&eacute;los  pr&eacute;sent &agrave; une station</h1>
+            <h1>Informations sur les v&eacute;los  pr&eacute;sent &agrave; une station</h1>
         </div>
         
+        <br>
         <!-- Récupération des données -->
         
         <form method = "post">
             <p>
 				<label>Num&eacute;ro de la station :</label>
-                <input type="text" name="prenom" />
+                <input type="text" name="id_station" />
                 <input type="submit" value="Valider" />
+                <input type="submit" value="?" name="liste" />
             </p>
         </form>
 		
-
+		<br>
 		<!-- Requete -->
+
         <?php 
         
-        if (!empty($_POST['prenom'])) 
+		
+		if (!empty($_POST['liste'])) {
+
+			echo "<h3>Listes des stations possibles</h3>";
+			echo "<b>ID_station</b><br>";
+			include "connect.php"; 	
+            $requete = "SELECT * FROM stations;";
+            // Si l'execution est reussie... 
+            if($res = $connection->query($requete))
+            // ... on récupère un tableau stockant le résultat 
+				while ($station =  $res->fetch_assoc()) {
+					echo $station['id_station']."<br>";
+				}
+		
+             //fermeture de la connexion avec la base
+             $connection->close();
+		}
+        
+        if (!empty($_POST['id_station'])) 
         {
-			echo "V&eacute;los  pr&eacute;sent &agrave; la station num&eacute;ro ".$_POST['prenom']." :\n";
+			echo "<h3>V&eacute;los  pr&eacute;sent &agrave; la station num&eacute;ro ".$_POST['id_station']." :\n</h3>";
 		?>
 		<table>
 			<tr>
-		<th>id</th>
-		<th>marque</th>
+		        <th>ID</th>
+		        <th>Marque</th>
+		        <th>Etat</th>
 			</tr>
 
 		<?php
 			include "connect.php"; 	
-            $requete = "SELECT velos.* FROM velos INNER JOIN stations on velos.id_station = stations.id_station WHERE stations.id_station=".$_POST['prenom'].";";
+            $requete = "SELECT velos.* FROM velos INNER JOIN stations on velos.id_station = stations.id_station WHERE stations.id_station=".$_POST['id_station'].";";
             // Si l'execution est reussie... 
             if($res = $connection->query($requete))
             // ... on récupère un tableau stockant le résultat 
 				while ($velo =  $res->fetch_assoc()) {
 					echo "\t".'<tr><td>'.$velo['id_velo'].'</td>';
 					echo '<td>'.$velo['marque'].'</td>';
+					echo '<td>'.$velo['etat'].'</td>';
 					echo '</tr>'."\n";
                 }
 		
@@ -56,6 +79,38 @@
         
          ?>
          </table>
+         <br>
+         <?php 
+        
+        if (!empty($_POST['id_station'])) 
+        {
+			echo "<h3>Classement des v&eacute;los les plus charg&eacute;s &agrave; la station num&eacute;ro ".$_POST['id_station']." :\n</h3>";
+		?>
+		<table>
+			<tr>
+		        <th>ID</th>
+		        <th>Batterie</th>
+			</tr>
+
+		<?php
+			include "connect.php"; 	
+            $requete = "SELECT id_velo, batterie FROM velos INNER JOIN stations on velos.id_station = stations.id_station WHERE stations.id_station=".$_POST['id_station']." ORDER BY batterie DESC;";
+            // Si l'execution est reussie... 
+            if($res = $connection->query($requete))
+            // ... on récupère un tableau stockant le résultat 
+				while ($velo =  $res->fetch_assoc()) {
+					echo "\t".'<tr><td>'.$velo['id_velo'].'</td>';
+					echo '<td>'.$velo['batterie'].'</td>';
+					echo '</tr>'."\n";
+                }
+		
+             //fermeture de la connexion avec la base
+             $connection->close();
+        } 
+        
+         ?>
+         </table>
+         
    </body>
 </html>
 
