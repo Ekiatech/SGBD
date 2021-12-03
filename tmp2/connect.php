723495eb0e925be614451a5519214fda7cd576a1
Coralie Deplanne <cdeplanne001@enseirb-matmeca.fr>
2021-12-03 18:11:12 +0100

    quelques modifs

---
+++ tmp2/connect.php
@@ -0,0 +1,6 @@
+ <?php
+  $login = 'cdeplanne001'/*A compléter*/;
+  $db_pwd = 'se17zifu'/*A compléter*/;
+  /* Creation de l'objet qui gere la connexion: */
+  $connection = new mysqli("localhost", $login, $db_pwd, $login);
+?> 
