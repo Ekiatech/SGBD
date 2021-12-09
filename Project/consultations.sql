da4032d7e8650dbdf3987cb0773f15825a94d87c
PAUWELS Quentin <Quentin.Pauwels@bordeaux-inp.fr>
2021-12-09 12:15:32 +0100

    I think it's the last one, isn't it ?

--- Project/consultations.sql
+++ Project/consultations.sql
@@ -95,19 +95,19 @@ SELECT id_adherent FROM (SELECT id_adherent, count(*) as nbr FROM (SELECT id_adh
 SELECT id_station_debut, count(*) as nbr_utilisations FROM utilisations GROUP BY id_station_debut ORDER BY nbr_utilisations DESC;
 
 /* STATION FIN */
-SELECT id_station_fin, count(*) as nbr_utilisations FROM utilisations GROUP BY id_station_fin ORDER BY nbr_utilisations DESC;
+SELECT id_station_fin, count(*) as nbr_utilisations FROM utilisations WHERE id_station_fin IS NOT NULL GROUP BY id_station_fin ORDER BY nbr_utilisations DESC;
 
 /** CLASSEMENT VELOS LES + UTILISES **/
 SELECT id_velo, count(*) as nbr_utilisations FROM utilisations GROUP BY id_velo ORDER BY nbr_utilisations DESC;
 
 /** MOYENNE DISTANCE PARCOURU/SEMAINE **/
-SELECT week, (km / nbr_adh) as nbr_km_moyenne_par_adherentFROM (SELECT count(*) as nbr_adh FROM adherents WHERE date_fin_adhesion >= DATE(NOW())) as a, (SELECT weekofyear(date_debut) as week, SUM(kilometrage_parcouru) as km FROM utilisations GROUP BY weekofyear(date_debut)) as b;
+SELECT week, (km / nbr_adh) as nbr_km_moyenne_par_adherent FROM (SELECT count(*) as nbr_adh FROM adherents WHERE date_fin_adhesion >= DATE(NOW())) as a, (SELECT weekofyear(date_debut) as week, SUM(kilometrage_parcouru) as km FROM utilisations GROUP BY weekofyear(date_debut)) as b;
 
 /** RAPPORT RENOUVELLEMENT ABONNEMENT **/
 CALL taux_reabonnement();
 
 /** CLASSEMENT TRAJET PLUS EFFECTUE **/
-SELECT id_station_debut, id_station_fin, count(*) as nbr_de_fois_effectue from utilisations GROUP BY id_station_debut, id_station_fin;
+SELECT id_station_debut, id_station_fin, count(*) as nbr_de_fois_effectue FROM utilisations WHERE id_station_fin IS NOT NULL GROUP BY id_station_debut, id_station_fin ORDER BY nbr_de_fois_effectue DESC;
 
 /** MOYENNE NOMBRE D'USAGER PAR VELO PAR JOUR  **/
 CALL avg_nbr_usager_velo_jour(p_id_velo);
